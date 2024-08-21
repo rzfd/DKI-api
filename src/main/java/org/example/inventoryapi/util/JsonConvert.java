9fa49968e5e279273698f418fbbf44369d1ac67c
rzfd <rizkifadillah783@gmail.com>
2024-08-21 13:01:57 +0700

    Feat: Add JSON Convert Util

---
+++ src/main/java/org/example/inventoryapi/util/JsonConvert.java
@@ -0,0 +1,42 @@
+package org.example.inventoryapi.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Converter(autoApply = true)
+public class JsonConvert implements AttributeConverter<Map<String, Object>, String> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting Map to JSON", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        if (dbData == null) {
+            return null;
+        }
+        try {
+            return objectMapper.readValue(dbData, Map.class);
+        } catch (JsonMappingException e) {
+            throw new IllegalArgumentException("Error mapping JSON to Map", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Error reading JSON", e);
+        }
+    }
+}
